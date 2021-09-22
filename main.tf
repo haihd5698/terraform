701724d75e76640b01a8dfa484f7224abf3406ae
haihd5698 <hai56.ntg@gmail.com>
2021-09-23 00:41:30 +0700

    remove access key

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "ap-southeast-1"
-    access_key = "AKIAUQA7DY67NC2NVGUY"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 
 }
