61d8d598348e1daba95f2342f21f852620fb40e3
haihd5698 <hai56.ntg@gmail.com>
2021-09-22 08:53:08 +0700

    upload file

---
+++ config_server.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+sudo apt update -y
+sudo apt install nginx php-mysql php-fpm -y
+cd cd /var/www/
+git clone https://github.com/WordPress/WordPress.git wordpress
+sudo chmod 777 -R wordpress
+sudo echo "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/wordpress;
+        index index.html index.htm index.php index.nginx-debian.html;
+        server_name _;
+        location / {
+                try_files $uri $uri/ =404;
+        }
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
+
+        }
+}" > /etc/nginx/sites-available/default
+sudo systemctl restart nginx.service
